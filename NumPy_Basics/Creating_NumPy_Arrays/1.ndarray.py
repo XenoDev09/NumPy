194c5c48e164462bf7522aa37aec8f29d6cbad18
XenoDev09 <nischal789987@gmail.com>
2025-08-02 19:30:20 +0545

    commited

---
+++ NumPy_Basics/Creating_NumPy_Arrays/1.ndarray.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = np.array([1,2,3]) # 1D array
+
+y = np.array([[1,2],
+              [3,4]]) # 2D array
+
+z = np.array([[[1,2],[3,4]],
+              [[5,6],[7,8]]]) # 3D array
+
+print("Printing 1D array:",x)
+print("Printing 2D array:",y)
+print("Printing 3D array:",z)
---
+++ NumPy_Basics/Creating_NumPy_Arrays/2.numpy_functions.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a1_zeros = np.zeros((3,3))
+
+a2_ones = np.ones((2,2))
+
+a3_range = np.arange(0,10,2)
+
+print(a1_zeros)
+print(a2_ones)
+print(a3_range)
